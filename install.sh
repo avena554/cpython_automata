f7c9df0fa0e9e5dcf1f4b0cec6a8e543ee4fd92b
antoine <antoine@sherekhan>
2023-07-21 15:42:09 -0400

    force resintall built packages

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ echo "Fetching PyPA's build if needed"
 python3 -m pip install --upgrade build
 echo "Building Pyta lib"
 cd python
-python3 -m build
+python3 -m build 
 echo "Installing"
-python3 -m pip install dist/pyta-*.whl
+python3 -m pip install dist/pyta-*.whl --force-reinstall
 cd ..
 echo "Building and installing C extension"
 make -C c_backend install
